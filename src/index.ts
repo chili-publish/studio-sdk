8726aaaee2849d9129f34381e7c0d4bd9687abc0
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2021-08-23 10:51:12 +0100

    Add CHILIProvder to the list of exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export { default as hello } from './interactions/hello';
 export { default as Editor } from './components/editor/Editor';
+export { default as Provider } from 'readline';
+export { store } from './store';
+export { default as CHILIProvider } from './components/chiliProvider';
