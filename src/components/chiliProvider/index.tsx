dbfd8c81f6549d14c887767942215af5da067a4d
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2021-08-23 15:02:27 +0100

    remove //eslint-disable-next-line max-len from CHILIProvider

--- src/components/chiliProvider/index.tsx
+++ src/components/chiliProvider/index.tsx
@@ -2,6 +2,5 @@ import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../store';
 
-// eslint-disable-next-line max-len
 const CHILIProvider: FC<ReactNode> = ({ children }) => <Provider store={store}>{children}</Provider>;
 export default CHILIProvider;
