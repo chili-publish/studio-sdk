eb36bc619ddaa8c542331c51be77a1ff11f6d62b
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2021-08-25 14:52:47 +0100

    add the div where the iframe will show up

--- src/components/editor/EditorWrapper.tsx
+++ src/components/editor/EditorWrapper.tsx
@@ -4,6 +4,6 @@ interface PropsType {
     editorLink: string;
 }
 
-const EditorWrapper: FC<PropsType> = ({ editorLink }) => <iframe title="editor 2" frameBorder={0} src={editorLink} />;
+const EditorWrapper: FC<PropsType> = ({ editorLink }) => <div id="iframe" style={{ height: '100%' }} />;
 
 export default EditorWrapper;
