ccdd00cbdb15c865d99cf222c0cee3238ce25830
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2021-08-26 11:12:02 +0100

    remove unnecessary files

--- src/components/editor/EditorWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-interface PropsType {
-    editorLink: string;
-}
-
-const EditorWrapper: FC<PropsType> = ({ editorLink }) => <div id="iframe" style={{ height: '100%' }} />;
-
-export default EditorWrapper;
