40f3e18e329bfa4446598b93e8718391a467bf17
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2021-09-06 15:44:29 +0100

    change the state type to be same with what we get from Flutter

--- types/CommonTypes.ts
+++ types/CommonTypes.ts
@@ -1,7 +1,7 @@
 import { CallSender } from 'penpal';
 
 export type ConfigType = {
-    stateChanged: (state: Document) => void;
+    stateChanged: (state: string) => void;
     editorLink: string;
 };
 
@@ -9,4 +9,5 @@ export interface Child extends CallSender {
     addLayout: (parentId: number) => void;
     removeLayout: (id: number) => void;
     renameLayout: (id: number, layoutName: string) => void;
+    selectLayout: (id: number) => Promise<{ success: boolean; status: number }>;
 }
