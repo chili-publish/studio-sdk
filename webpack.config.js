58827cd9934112dbcd94b585224c4f3e72ca364f
brapoprod <broels@chili-publish.com>
2021-10-12 09:40:36 +0200

    [Fix] Add bundle optimalisation using terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = (env) => ({
     entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -37,6 +39,15 @@ module.exports = (env) => ({
             amd: 'ReactDOM',
             root: 'ReactDOM',
         },
+    },
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
     },
     devtool: 'source-map',
 });
