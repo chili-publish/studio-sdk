5b8c73795e1b1e2c35d05040acd9d7ae39c35cfd
Brian Roels <54845569+brapoprod@users.noreply.github.com>
2021-11-02 14:16:11 +0100

    [Feature] Add animation typing (#34)

---
+++ types/AnimationTypes.ts
@@ -0,0 +1,99 @@
+export enum SlideDirections {
+    top = 'top',
+    left = 'left',
+    right = 'right',
+    bottom = 'bottom',
+    topLeft = 'topLeft',
+    topRight = 'topRight',
+    bottomLeft = 'bottomLeft',
+    bottomRight = 'bottomRight',
+}
+
+export enum ShakeDirections {
+    horizontal = 'horizontal',
+    vertical = 'vertical',
+}
+
+export enum EaseTypes {
+    easeIn = 'easeIn',
+    easeOut = 'easeOut',
+    easeInOut = 'easeInOut',
+}
+
+export enum TweenTypes {
+    quadratic = 'Quadratic',
+    cubic = 'Cubic',
+    quartic = 'Quartic',
+    quintic = 'Quintic',
+    sine = 'Sine',
+    exponential = 'Exponential',
+    circular = 'Circular',
+    elastic = 'Elastic',
+    bounce = 'Bounce',
+    back = 'Back',
+}
+
+export type EaseTweenCombinationType = `${EaseTypes}${TweenTypes}`;
+
+export type BasicAnimationsIntroOutroStylesType = {
+    slide?: {
+        slideFrom?: SlideDirections;
+        slideTo?: SlideDirections;
+        offsetPercent: number;
+    };
+    rotation?: {
+        angleDegrees: number;
+        numberOfRotations?: number;
+    };
+    scale?: {
+        scalePercent: number;
+    };
+    fade?: boolean;
+};
+
+export type BasicAnimationsIntroType = {
+    from: number;
+    to: number;
+    ease: EaseTweenCombinationType;
+    styles: BasicAnimationsIntroOutroStylesType;
+};
+
+export type BasicAnimationsMiddleType = {
+    from: number;
+    to: number;
+    ease: EaseTweenCombinationType;
+    styles: {
+        bounce?: boolean;
+        flash?: boolean;
+        pulse?: boolean;
+        rubberBand?: boolean;
+        sbake?: {
+            shakeDirection: ShakeDirections;
+        };
+        headShake?: boolean;
+        swing?: boolean;
+        tada?: boolean;
+        heartBeat?: boolean;
+    };
+};
+
+export type BasicAnimationsOutroType = {
+    from: number;
+    to: number;
+    ease: EaseTweenCombinationType;
+    styles: BasicAnimationsIntroOutroStylesType;
+};
+
+export type BasicAnimationsType = {
+    intro?: BasicAnimationsIntroType;
+    middle?: BasicAnimationsMiddleType;
+    outro?: BasicAnimationsOutroType;
+};
+
+export type FrameAnimationType = {
+    frameId: number;
+    from: number;
+    to: number;
+    basicAnimations: BasicAnimationsType;
+    advancedAnimations?: unknown; // TBI
+};
