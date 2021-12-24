a8383fb684d78478d4ed4a3b10d04edb91d0718d
brapoprod <broels@chili-publish.com>
2021-12-24 09:00:51 +0100

    Add tests

--- src/tests/controllers/AnimationController.test.ts
+++ src/tests/controllers/AnimationController.test.ts
@@ -23,6 +23,9 @@ beforeEach(() => {
     mockedSDK.animation.setFrameAnimation = defaultMockReturn;
     mockedSDK.animation.playAnimation = defaultMockReturn;
     mockedSDK.animation.pauseAnimation = defaultMockReturn;
+    mockedSDK.animation.setScrubberPosition = defaultMockReturn;
+    mockedSDK.animation.setAnimationDuration = defaultMockReturn;
+    mockedSDK.animation.onAnimationPlaybackChanged = defaultMockReturn;
 });
 
 afterEach(() => {
@@ -41,5 +44,17 @@ describe('Animation methods', () => {
 
         await mockedSDK.animation.pauseAnimation();
         expect(mockedSDK.animation.pauseAnimation).toHaveBeenCalledTimes(4);
+
+        await mockedSDK.animation.setScrubberPosition(5000);
+        expect(mockedSDK.animation.setScrubberPosition).toHaveBeenCalledTimes(5);
+        expect(mockedSDK.animation.setScrubberPosition).toHaveBeenLastCalledWith(5000);
+
+        await mockedSDK.animation.setAnimationDuration(8000);
+        expect(mockedSDK.animation.setAnimationDuration).toHaveBeenCalledTimes(6);
+        expect(mockedSDK.animation.setAnimationDuration).toHaveBeenLastCalledWith(8000);
+
+        mockedSDK.animation.onAnimationPlaybackChanged('test');
+        expect(mockedSDK.config.scrubberPositionChanged).toHaveBeenCalledTimes(7);
+        expect(mockedSDK.config.scrubberPositionChanged).toHaveBeenCalledWith('test');
     });
 });
