e4f47298853b89d14d0e655750ac366904d6533a
brapoprod <broels@chili-publish.com>
2021-12-27 10:57:22 +0100

    Add unittests

---
+++ src/tests/utils/getClaculatedValue.test.ts
@@ -0,0 +1,44 @@
+import { getCalculatedValue } from '../../utils/getCalculatedValue';
+
+describe('getCalculatedValue', () => {
+    it('calculates the correct value from a string with a sum operation', () => {
+        const value = getCalculatedValue('1 + 1');
+        expect(value).toBe(2);
+    });
+    it('calculates the correct value from a string with a substract operation', () => {
+        let value = getCalculatedValue('1 - 1');
+        expect(value).toBe(0);
+
+        value = getCalculatedValue('15 - 3');
+        expect(value).toBe(12);
+    });
+    it('calculates the correct value from a string with a multiply operation', () => {
+        let value = getCalculatedValue('1 * 1');
+        expect(value).toBe(1);
+
+        value = getCalculatedValue('5 * 3');
+        expect(value).toBe(15);
+    });
+    it('calculates the correct value from a string with a division operation', () => {
+        const value = getCalculatedValue('10 / 2');
+        expect(value).toBe(5);
+    });
+
+    it('calculates multiple operators, in correct order of operations', () => {
+        const value = getCalculatedValue('10 / 2 + 5 - 3 * 3');
+        expect(value).toBe(1);
+    });
+    it('rounds the result', () => {
+        let value = getCalculatedValue('1 * 1,1254');
+        expect(value).toBe(1.13);
+
+        value = getCalculatedValue('1 * 0,999');
+        expect(value).toBe(1);
+
+        value = getCalculatedValue('1 * 0,999', 3);
+        expect(value).toBe(0.999);
+
+        value = getCalculatedValue('1 * 0,999', 4);
+        expect(value).toBe(0.999);
+    });
+});
--- src/utils/getCalculatedValue.ts
+++ src/utils/getCalculatedValue.ts
@@ -1,11 +1,18 @@
 import { evaluate, round } from 'mathjs';
 
-export const getCalculatedValue = (value: string) => {
+/*
+    TODO:
+    - map calculations
+    - calculate
+    - round
+*/
+
+export const getCalculatedValue = (value: string, roundPrecision = 2) => {
     const str = value.replace(/[^0-9,\-,+,/,*,(,),/,/,/./]/gi, '').replace(/,/gi, '.');
     if (str === null || str.length === 0) return null;
     let calc: number | null;
     try {
-        calc = round(evaluate(str), 2);
+        calc = round(evaluate(str), roundPrecision);
     } catch (error) {
         calc = null;
     }
