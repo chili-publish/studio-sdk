716f3f9bf96eab17e6008bef0fc8c5be0bc265ae
Pieter Van Parys <pieter.vanparys@gmail.com>
2022-09-29 09:05:42 +0200

    [Feature] Bump Editor Engine version

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ export { ColorType } from '../types/ColorStyleTypes';
 
 let connection: Connection;
 
-const FIXED_EDITOR_LINK = 'https://studio-cdn.chiligrafx.com/editor/0.0.6/web';
+const FIXED_EDITOR_LINK = 'https://studio-cdn.chiligrafx.com/editor/0.0.7/web';
 
 export class SDK {
     config: ConfigType;
