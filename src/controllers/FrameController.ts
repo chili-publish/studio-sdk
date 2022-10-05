245ae00c839c0e48a57c79b3b6a33ff6db6e970a
brapoprod <broels@chili-publish.com>
2022-10-05 13:02:53 +0200

    Explicit imageFrameId

--- src/controllers/FrameController.ts
+++ src/controllers/FrameController.ts
@@ -316,13 +316,15 @@ export class FrameController {
 
     /**
      * This method will assign an image from the mediaConnector to the correct imageFrame
-     * @param frameId The ID of the imageFrame where an image needs to be assigned to
+     * @param imageFrameId The ID of the imageFrame where an image needs to be assigned to
      * @param connectorId Unique Id of the media connector
      * @param imageId Unique Id of the image that you want to assign to the imageFrame
      * @returns
      */
-    assignImageToFrame = async (frameId: number, connectorId: string, imageId: string) => {
+    assignImageToFrame = async (imageFrameId: number, connectorId: string, imageId: string) => {
         const res = await this.#editorAPI;
-        return res.assignImage(frameId, connectorId, imageId).then((result) => getEditorResponseData<null>(result));
+        return res
+            .assignImage(imageFrameId, connectorId, imageId)
+            .then((result) => getEditorResponseData<null>(result));
     };
 }
