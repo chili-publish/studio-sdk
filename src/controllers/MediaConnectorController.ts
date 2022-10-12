f10975c1cbb8e2f7944a7387aceb29d430d832ce
Laurent Raymond <1528484+Dvergar@users.noreply.github.com>
2022-10-12 17:54:45 +0200

    authentication doc fixes

--- src/controllers/MediaConnectorController.ts
+++ src/controllers/MediaConnectorController.ts
@@ -180,8 +180,7 @@ export class ConnectorAuthenticationController {
 
     /**
      * This method sets the CHILI GraFx Access Token in the Authentication HTTP header for the 'chili' authentication type.
-     * The CHILI Token will be used in every connector fetch call.
-     * Gives access to the CHILI environment API.
+     * The CHILI Token will be used to authenticate every grafx-media connector http call.
      * Can only be used after a connector has been registered.
      * @param connectorId unique Id of the media connector
      * @param token token for the CHILI authentication
@@ -194,7 +193,7 @@ export class ConnectorAuthenticationController {
 
     /**
      * This method sets the HTTP headers for the 'staticKey' authentication type.
-     * Those authentication headers will be used in every connector fetch call.
+     * These additional headers will be added to all connector http calls.
      * Can only be used after a connector has been registered.
      * @param connectorId unique Id of the media connector
      * @param headerName name of the header
