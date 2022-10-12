c5c0c3d0b559aa41fd35d4d533de96e19c3a3500
Laurent Raymond <1528484+Dvergar@users.noreply.github.com>
2022-10-12 09:23:58 +0200

    [Feature] Add authentication methods

--- src/controllers/MediaConnectorController.ts
+++ src/controllers/MediaConnectorController.ts
@@ -28,6 +28,7 @@ export class MediaConnectorController {
      */
     #editorAPI: EditorAPI;
     #blobAPI: EditorRawAPI;
+    authentication: ConnectorAuthenticationController;
 
     /**
      * @ignore
@@ -35,6 +36,7 @@ export class MediaConnectorController {
     constructor(editorAPI: EditorAPI) {
         this.#editorAPI = editorAPI;
         this.#blobAPI = editorAPI as CallSender as EditorRawAPI;
+        this.authentication = new ConnectorAuthenticationController(editorAPI);
     }
 
     /**
@@ -158,3 +160,45 @@ export class MediaConnectorController {
         if (deprecatedType === DeprecatedMediaType.collection) return MediaType.collection;
     };
 }
+
+/**
+ * The ConnectorAuthenticationController is responsible for authenticating regarding media connectors.
+ * Methods inside this controller can be called by `window.SDK.mediaConnector.authentication.{method-name}`
+ */
+export class ConnectorAuthenticationController {
+    /**
+     * @ignore
+     */
+    #editorAPI: EditorAPI;
+
+    /**
+     * @ignore
+     */
+    constructor(editorAPI: EditorAPI) {
+        this.#editorAPI = editorAPI;
+    }
+
+    /**
+     * This method sets the token for the 'chili' authentication type.
+     * @param connectorId unique Id of the media connector
+     * @param token token for the chili authentication
+     */
+    setChiliToken = async (connectorId: string, token: string) => {
+        const res = await this.#editorAPI;
+        return res.connectorAuthenticationSetChiliToken(connectorId, token)
+            .then((result) => getEditorResponseData<null>(result));
+    }
+
+    /**
+     * This method sets the http headers for the 'staticKey' authentication type.
+     * @param connectorId unique Id of the media connector
+     * @param headerName name of the header
+     * @param headerValue value of the header
+     */
+    setHttpHeader = async (connectorId: string, headerName: string, headerValue: string) => {
+        const res = await this.#editorAPI;
+        return res.connectorAuthenticationSetHttpHeader(connectorId, headerName, headerValue)
+            .then((result) => getEditorResponseData<null>(result));
+    }
+}
+
