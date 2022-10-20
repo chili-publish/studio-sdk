50bbd19acbd9e6438a8bd5821757fa86d86b1d23
Alexandra Bri <popalexandra45@gmail.com>
2022-10-20 09:46:00 +0300

    Fixes after merge

--- src/tests/controllers/FrameController.test.ts
+++ src/tests/controllers/FrameController.test.ts
@@ -1,6 +1,6 @@
-import {FitMode, VerticalAlign} from '../../../types/FrameTypes';
-import {FrameController} from '../../controllers/FrameController';
-import {mockSelectFrame} from '../__mocks__/FrameProperties';
+import { FitMode, VerticalAlign } from '../../../types/FrameTypes';
+import { FrameController } from '../../controllers/FrameController';
+import { mockSelectFrame } from '../__mocks__/FrameProperties';
 import MockEditorAPI from '../__mocks__/MockEditorAPI';
 
 let mockedFrameProperties: FrameController;
