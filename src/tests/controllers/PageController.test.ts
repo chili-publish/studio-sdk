734b5553bb73317c11df176db68efb72b97191a6
brapoprod <broels@chili-publish.com>
2023-03-02 13:29:50 +0100

    Refactor PageController tests

--- src/tests/controllers/PageController.test.ts
+++ src/tests/controllers/PageController.test.ts
@@ -1,24 +1,40 @@
+import { EditorAPI } from '../../../types/CommonTypes';
+import { Page } from '../../../types/PageTypes';
 import { PageController } from '../../controllers/PageController';
-import MockEditorAPI from '../__mocks__/MockEditorAPI';
+import { castToEditorResponse, getEditorResponseData } from '../../utils/EditorResponseData';
 
 let mockedPageProperties: PageController;
 
+const mockPages: Page[] = [
+    {
+        pageId: 'aaa',
+        pageNumber: 0,
+        width: 100,
+        height: 200,
+    },
+];
+
+const mockEditorApi: EditorAPI = {
+    getPages: async () => getEditorResponseData(castToEditorResponse(mockPages)),
+    getPageById: async (id: unknown) => getEditorResponseData(castToEditorResponse(id)),
+};
+
 beforeEach(() => {
-    mockedPageProperties = new PageController(MockEditorAPI);
-    jest.spyOn(mockedPageProperties, 'getPages');
-    jest.spyOn(mockedPageProperties, 'getPageById');
+    mockedPageProperties = new PageController(mockEditorApi);
+    jest.spyOn(mockEditorApi, 'getPages');
+    jest.spyOn(mockEditorApi, 'getPageById');
 });
 
 afterAll(() => {
     jest.restoreAllMocks();
 });
 describe('PageProperties', () => {
-    it('Should call all of the Page Functions of EditorAPI successfully', () => {
-        mockedPageProperties.getPages();
-        expect(mockedPageProperties.getPages).toHaveBeenCalledTimes(1);
+    it('Should call all of the Page Functions of EditorAPI successfully', async () => {
+        await mockedPageProperties.getPages();
+        expect(mockEditorApi.getPages).toHaveBeenCalledTimes(1);
 
-        mockedPageProperties.getPageById('4');
-        expect(mockedPageProperties.getPageById).toHaveBeenCalledTimes(1);
-        expect(mockedPageProperties.getPageById).toHaveBeenCalledWith('4');
+        await mockedPageProperties.getPageById('4');
+        expect(mockEditorApi.getPageById).toHaveBeenCalledTimes(1);
+        expect(mockEditorApi.getPageById).toHaveBeenCalledWith('4');
     });
 });
