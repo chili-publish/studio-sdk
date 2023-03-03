b6d8e1c0f6b5e50a86138bbb04cb4576fafdf2e6
brapoprod <broels@chili-publish.com>
2023-03-03 17:21:23 +0100

    Fix Connector, Debug and Documentcontroller tests

--- src/tests/controllers/DebugController.test.ts
+++ src/tests/controllers/DebugController.test.ts
@@ -1,28 +1,36 @@
+import { EditorAPI } from '../../../types/CommonTypes';
 import { DebugController } from '../../controllers/DebugController';
-import MockEditorAPI from '../__mocks__/MockEditorAPI';
+import { getEditorResponseData, castToEditorResponse } from '../../utils/EditorResponseData';
 
-let mockedDebugProperties: DebugController;
+let mockedDebugController: DebugController;
+
+const mockedEditorApi: EditorAPI = {
+    getLogs: async () => getEditorResponseData(castToEditorResponse(null)),
+    toggleDebugPanel: async () => getEditorResponseData(castToEditorResponse(null)),
+    enableDebug: async () => getEditorResponseData(castToEditorResponse(null)),
+    disableDebug: async () => getEditorResponseData(castToEditorResponse(null)),
+};
 
 beforeEach(() => {
-    mockedDebugProperties = new DebugController(MockEditorAPI);
-    jest.spyOn(mockedDebugProperties, 'getLogs');
-    jest.spyOn(mockedDebugProperties, 'toggleDebugPanel');
-    jest.spyOn(mockedDebugProperties, 'enableDebug');
-    jest.spyOn(mockedDebugProperties, 'disableDebug');
+    mockedDebugController = new DebugController(mockedEditorApi);
+    jest.spyOn(mockedEditorApi, 'getLogs');
+    jest.spyOn(mockedEditorApi, 'toggleDebugPanel');
+    jest.spyOn(mockedEditorApi, 'enableDebug');
+    jest.spyOn(mockedEditorApi, 'disableDebug');
 });
 
 afterAll(() => {
     jest.restoreAllMocks();
 });
 describe('DebugProperties', () => {
-    it('Should call all of the Debug Functions of EditorAPI successfully', () => {
-        mockedDebugProperties.getLogs();
-        expect(mockedDebugProperties.getLogs).toHaveBeenCalledTimes(1);
-        mockedDebugProperties.toggleDebugPanel();
-        expect(mockedDebugProperties.toggleDebugPanel).toHaveBeenCalledTimes(1);
-        mockedDebugProperties.enableDebug();
-        expect(mockedDebugProperties.enableDebug).toHaveBeenCalledTimes(1);
-        mockedDebugProperties.disableDebug();
-        expect(mockedDebugProperties.disableDebug).toHaveBeenCalledTimes(1);
+    it('Should call all of the Debug Functions of EditorAPI successfully', async () => {
+        await mockedDebugController.getLogs();
+        expect(mockedEditorApi.getLogs).toHaveBeenCalledTimes(1);
+        await mockedDebugController.toggleDebugPanel();
+        expect(mockedEditorApi.toggleDebugPanel).toHaveBeenCalledTimes(1);
+        await mockedDebugController.enableDebug();
+        expect(mockedEditorApi.enableDebug).toHaveBeenCalledTimes(1);
+        await mockedDebugController.disableDebug();
+        expect(mockedEditorApi.disableDebug).toHaveBeenCalledTimes(1);
     });
 });
