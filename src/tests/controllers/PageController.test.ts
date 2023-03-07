37a7d9323c7281273d7bae1788bf4cf51a33ec6a
brapoprod <broels@chili-publish.com>
2023-03-07 14:18:27 +0100

    Extend pageController tests

--- src/tests/controllers/PageController.test.ts
+++ src/tests/controllers/PageController.test.ts
@@ -23,20 +23,33 @@ afterAll(() => {
     jest.restoreAllMocks();
 });
 describe('PageController', () => {
-    it('Should call all of the Page Functions of EditorAPI successfully', async () => {
+    it('Should call the getPages method', async () => {
         await mockedPageProperties.getPages();
         expect(mockEditorApi.getPages).toHaveBeenCalledTimes(1);
-
+    });
+    it('Should call the getPageById method', async () => {
         await mockedPageProperties.getPageById('4');
         expect(mockEditorApi.getPageById).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.getPageById).toHaveBeenCalledWith('4');
-
+    });
+    it('Should call the setPageWidth method and transform string to number', async () => {
         await mockedPageProperties.setPageWidth('id', '4');
         expect(mockEditorApi.setPageWidth).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setPageWidth).toHaveBeenCalledWith('id', 4);
-
+    });
+    it('Should call the setPageHeight method and transform string to number', async () => {
         await mockedPageProperties.setPageHeight('id', '4');
         expect(mockEditorApi.setPageHeight).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setPageHeight).toHaveBeenCalledWith('id', 4);
+    });
+
+    it('Should accept calculations for the pageHeight and pageWidth methods', async () => {
+        await mockedPageProperties.setPageHeight('id', '4+2');
+        expect(mockEditorApi.setPageHeight).toHaveBeenCalledTimes(2);
+        expect(mockEditorApi.setPageHeight).toHaveBeenCalledWith('id', 6);
+
+        await mockedPageProperties.setPageWidth('id', '4*3');
+        expect(mockEditorApi.setPageWidth).toHaveBeenCalledTimes(2);
+        expect(mockEditorApi.setPageWidth).toHaveBeenCalledWith('id', 12);
     });
 });
