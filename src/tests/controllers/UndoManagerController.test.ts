8ea94379b1686fed4bac2e0b9c01271f24179f4d
brapoprod <broels@chili-publish.com>
2023-03-07 16:00:55 +0100

    Refactor UndoManagerController

---
+++ src/tests/controllers/UndoManagerController.test.ts
@@ -0,0 +1,32 @@
+import { UndoManagerController } from '../../controllers/UndoManagerController';
+import { EditorAPI } from '../../../types/CommonTypes';
+import { getEditorResponseData, castToEditorResponse } from '../../utils/EditorResponseData';
+
+describe('UndoManagerController', () => {
+    let mockedUndoManagerController: UndoManagerController;
+
+    const mockEditorApi: EditorAPI = {
+        undo: async () => getEditorResponseData(castToEditorResponse(null)),
+        redo: async () => getEditorResponseData(castToEditorResponse(null)),
+    };
+
+    beforeEach(() => {
+        mockedUndoManagerController = new UndoManagerController(mockEditorApi);
+        jest.spyOn(mockEditorApi, 'undo');
+        jest.spyOn(mockEditorApi, 'redo');
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('Undoes the last operation', async () => {
+        await mockedUndoManagerController.undo();
+        expect(mockEditorApi.undo).toHaveBeenCalledTimes(1);
+    });
+
+    it('Redoes the last operation', async () => {
+        await mockedUndoManagerController.redo();
+        expect(mockEditorApi.redo).toHaveBeenCalledTimes(1);
+    });
+});
