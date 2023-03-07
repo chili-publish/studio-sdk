993fc88c396985996dd56580d12cd2b7733281de
brapoprod <broels@chili-publish.com>
2023-03-07 13:51:20 +0100

    Refactor LayoutController

--- src/tests/controllers/LayoutController.test.ts
+++ src/tests/controllers/LayoutController.test.ts
@@ -1,33 +1,50 @@
-import { Id } from '../../../types/CommonTypes';
+import { EditorAPI, Id } from '../../../types/CommonTypes';
 import { LayoutController } from '../../controllers/LayoutController';
-import { SDK } from '../../index';
-import mockConfig from '../__mocks__/config';
+import { getEditorResponseData, castToEditorResponse } from '../../utils/EditorResponseData';
 import { mockSelectPage } from '../__mocks__/FrameProperties';
-import mockChild from '../__mocks__/MockEditorAPI';
 
-let mockedSDK: SDK;
+let mockedFontController: LayoutController;
 let mockId: Id;
+
+const mockedEditorApi: EditorAPI = {
+    getLayouts: async () => getEditorResponseData(castToEditorResponse(null)),
+    getLayoutById: async () => getEditorResponseData(castToEditorResponse(null)),
+    getLayoutByName: async () => getEditorResponseData(castToEditorResponse(null)),
+    getSelectedLayout: async () => getEditorResponseData(castToEditorResponse(null)),
+    removeLayout: async () => getEditorResponseData(castToEditorResponse(null)),
+    addLayout: async () => getEditorResponseData(castToEditorResponse(null)),
+    setLayoutName: async () => getEditorResponseData(castToEditorResponse(null)),
+    selectLayout: async () => getEditorResponseData(castToEditorResponse(null)),
+    duplicateLayout: async () => getEditorResponseData(castToEditorResponse(null)),
+    resetLayout: async () => getEditorResponseData(castToEditorResponse(null)),
+    setLayoutHeight: async () => getEditorResponseData(castToEditorResponse(null)),
+    setLayoutWidth: async () => getEditorResponseData(castToEditorResponse(null)),
+    resetLayoutHeight: async () => getEditorResponseData(castToEditorResponse(null)),
+    resetLayoutWidth: async () => getEditorResponseData(castToEditorResponse(null)),
+    getPageSnapshot: async () => getEditorResponseData(castToEditorResponse(null)),
+    renameLayout: async () => getEditorResponseData(castToEditorResponse(null)),
+};
+
 beforeEach(() => {
-    mockedSDK = new SDK(mockConfig);
-    jest.spyOn(mockedSDK.layout, 'getLayouts');
-    jest.spyOn(mockedSDK.layout, 'getLayoutById');
-    jest.spyOn(mockedSDK.layout, 'getLayoutByName');
-    jest.spyOn(mockedSDK.layout, 'getSelectedLayout');
-    jest.spyOn(mockedSDK.layout, 'removeLayout');
-    jest.spyOn(mockedSDK.layout, 'addLayout');
-    jest.spyOn(mockedSDK.layout, 'setLayoutName');
-    jest.spyOn(mockedSDK.layout, 'selectLayout');
-    jest.spyOn(mockedSDK.layout, 'duplicateLayout');
-    jest.spyOn(mockedSDK.layout, 'resetLayout');
+    mockedFontController = new LayoutController(mockedEditorApi);
+    jest.spyOn(mockedEditorApi, 'getLayouts');
+    jest.spyOn(mockedEditorApi, 'getLayoutById');
+    jest.spyOn(mockedEditorApi, 'getLayoutByName');
+    jest.spyOn(mockedEditorApi, 'getSelectedLayout');
+    jest.spyOn(mockedEditorApi, 'removeLayout');
+    jest.spyOn(mockedEditorApi, 'addLayout');
+    jest.spyOn(mockedEditorApi, 'setLayoutName');
+    jest.spyOn(mockedEditorApi, 'selectLayout');
+    jest.spyOn(mockedEditorApi, 'duplicateLayout');
+    jest.spyOn(mockedEditorApi, 'resetLayout');
 
-    jest.spyOn(mockedSDK.layout, 'setLayoutHeight');
-    jest.spyOn(mockedSDK.layout, 'setLayoutWidth');
-    jest.spyOn(mockedSDK.layout, 'resetLayoutHeight');
-    jest.spyOn(mockedSDK.layout, 'resetLayoutWidth');
-    jest.spyOn(mockedSDK.layout, 'getSelectedLayoutSnapshot');
+    jest.spyOn(mockedEditorApi, 'setLayoutHeight');
+    jest.spyOn(mockedEditorApi, 'setLayoutWidth');
+    jest.spyOn(mockedEditorApi, 'resetLayoutHeight');
+    jest.spyOn(mockedEditorApi, 'resetLayoutWidth');
+    jest.spyOn(mockedEditorApi, 'getPageSnapshot');
+    jest.spyOn(mockedEditorApi, 'renameLayout');
 
-    mockedSDK.editorAPI = mockChild;
-    mockedSDK.layout = new LayoutController(mockChild);
     mockId = mockSelectPage.layoutId;
 });
 
@@ -37,65 +54,65 @@ afterEach(() => {
 
 describe('Layout methods', () => {
     it('handles all layout methods', async () => {
-        await mockedSDK.layout.getLayouts();
-        expect(mockedSDK.editorAPI.getLayouts).toHaveBeenCalledTimes(1);
+        await mockedFontController.getLayouts();
+        expect(mockedEditorApi.getLayouts).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.getLayoutById('1');
-        expect(mockedSDK.editorAPI.getLayoutById).toHaveBeenCalledTimes(1);
-        expect(mockedSDK.editorAPI.getLayoutById).toHaveBeenCalledWith('1');
+        await mockedFontController.getLayoutById('1');
+        expect(mockedEditorApi.getLayoutById).toHaveBeenCalledTimes(1);
+        expect(mockedEditorApi.getLayoutById).toHaveBeenCalledWith('1');
 
-        await mockedSDK.layout.getLayoutByName('layout');
-        expect(mockedSDK.editorAPI.getLayoutByName).toHaveBeenCalledTimes(1);
-        expect(mockedSDK.editorAPI.getLayoutByName).toHaveBeenCalledWith('layout');
+        await mockedFontController.getLayoutByName('layout');
+        expect(mockedEditorApi.getLayoutByName).toHaveBeenCalledTimes(1);
+        expect(mockedEditorApi.getLayoutByName).toHaveBeenCalledWith('layout');
 
-        await mockedSDK.layout.getSelectedLayout();
-        expect(mockedSDK.editorAPI.getSelectedLayout).toHaveBeenCalledTimes(1);
+        await mockedFontController.getSelectedLayout();
+        expect(mockedEditorApi.getSelectedLayout).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.removeLayout('1');
-        expect(mockedSDK.editorAPI.removeLayout).toHaveBeenCalledTimes(1);
+        await mockedFontController.removeLayout('1');
+        expect(mockedEditorApi.removeLayout).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.addLayout('1');
-        expect(mockedSDK.editorAPI.addLayout).toHaveBeenCalledTimes(1);
+        await mockedFontController.addLayout('1');
+        expect(mockedEditorApi.addLayout).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.setLayoutName('1', 'TEST');
-        expect(mockedSDK.editorAPI.renameLayout).toHaveBeenCalledTimes(1);
+        await mockedFontController.setLayoutName('1', 'TEST');
+        expect(mockedEditorApi.renameLayout).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.selectLayout('1');
-        expect(mockedSDK.editorAPI.selectLayout).toHaveBeenCalledTimes(1);
+        await mockedFontController.selectLayout('1');
+        expect(mockedEditorApi.selectLayout).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.duplicateLayout('1');
-        expect(mockedSDK.editorAPI.duplicateLayout).toHaveBeenCalledTimes(1);
+        await mockedFontController.duplicateLayout('1');
+        expect(mockedEditorApi.duplicateLayout).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.resetLayout('1');
-        expect(mockedSDK.editorAPI.resetLayout).toHaveBeenCalledTimes(1);
+        await mockedFontController.resetLayout('1');
+        expect(mockedEditorApi.resetLayout).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.setLayoutHeight(mockId, '32');
-        expect(mockedSDK.editorAPI.setLayoutHeight).toHaveBeenCalledTimes(1);
+        await mockedFontController.setLayoutHeight(mockId, '32');
+        expect(mockedEditorApi.setLayoutHeight).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.setLayoutHeight(mockId, 'null');
-        expect(mockedSDK.editorAPI.setLayoutHeight).toHaveBeenCalledTimes(1);
+        await mockedFontController.setLayoutHeight(mockId, 'null');
+        expect(mockedEditorApi.setLayoutHeight).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.setLayoutWidth(mockId, '34');
-        expect(mockedSDK.editorAPI.setLayoutWidth).toHaveBeenCalledTimes(1);
+        await mockedFontController.setLayoutWidth(mockId, '34');
+        expect(mockedEditorApi.setLayoutWidth).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.setLayoutWidth(mockId, 'null');
-        expect(mockedSDK.editorAPI.setLayoutWidth).toHaveBeenCalledTimes(1);
+        await mockedFontController.setLayoutWidth(mockId, 'null');
+        expect(mockedEditorApi.setLayoutWidth).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.resetLayoutHeight('1');
-        expect(mockedSDK.editorAPI.resetLayoutHeight).toHaveBeenCalledTimes(1);
+        await mockedFontController.resetLayoutHeight('1');
+        expect(mockedEditorApi.resetLayoutHeight).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.resetLayoutWidth('1');
-        expect(mockedSDK.editorAPI.resetLayoutWidth).toHaveBeenCalledTimes(1);
+        await mockedFontController.resetLayoutWidth('1');
+        expect(mockedEditorApi.resetLayoutWidth).toHaveBeenCalledTimes(1);
 
-        await mockedSDK.layout.getSelectedLayoutSnapshot();
-        expect(mockedSDK.editorAPI.getPageSnapshot).toHaveBeenCalledTimes(1);
+        await mockedFontController.getSelectedLayoutSnapshot();
+        expect(mockedEditorApi.getPageSnapshot).toHaveBeenCalledTimes(1);
     });
 });
 
 describe('User inputs for Layout Properties', () => {
     it('Should calculate user Inputs and returns null when calculated value is null or same with selectedLayout property', async () => {
-        const responseHeight = await mockedSDK.layout.setLayoutHeight(mockId, 'fsadfafasf');
-        const responseWidth = await mockedSDK.layout.setLayoutWidth(mockId, '20/0');
+        const responseHeight = await mockedFontController.setLayoutHeight(mockId, 'fsadfafasf');
+        const responseWidth = await mockedFontController.setLayoutWidth(mockId, '20/0');
 
         expect(responseHeight).toEqual(null);
         expect(responseWidth).toEqual(null);
