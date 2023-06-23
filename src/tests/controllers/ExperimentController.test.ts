c373eaaf5deb0f2f8cdf131aae17a4835b0268de
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2023-06-23 13:36:48 +0200

    [NO-TICKET] [Fix] Use correct engine method `selectTextById` (#311)
    fix: use correct engine method `selectTextById`
    
    Co-authored-by: Pieter Van Parys <pieter.vanparys@gmail.com>
    Co-authored-by: Brian Roels <54845569+brapoprod@users.noreply.github.com>

--- src/tests/controllers/ExperimentController.test.ts
+++ src/tests/controllers/ExperimentController.test.ts
@@ -15,7 +15,7 @@ const mockedEditorApi: EditorAPI = {
     setImageSource: async () => getEditorResponseData(castToEditorResponse(null)),
     getTextByFrameId: async () => getEditorResponseData(castToEditorResponse(null)),
     setTextByFrameId: async () => getEditorResponseData(castToEditorResponse(null)),
-    selectTextByFrameId: async () => getEditorResponseData(castToEditorResponse(null)),
+    selectTextById: async () => getEditorResponseData(castToEditorResponse(null)),
 };
 
 beforeEach(() => {
@@ -27,7 +27,7 @@ beforeEach(() => {
     jest.spyOn(mockedEditorApi, 'setImageSource');
     jest.spyOn(mockedEditorApi, 'getTextByFrameId');
     jest.spyOn(mockedEditorApi, 'setTextByFrameId');
-    jest.spyOn(mockedEditorApi, 'selectTextByFrameId');
+    jest.spyOn(mockedEditorApi, 'selectTextById');
 
     frameId = mockSelectFrame.id;
 });
@@ -83,8 +83,7 @@ describe('ExperimentController', () => {
         const length = 3;
 
         await mockedExperimentController.selectText(frameId, startIndex, length);
-        expect(mockedEditorApi.selectTextByFrameId).toHaveBeenCalledTimes(1);
-        expect(mockedEditorApi.selectTextByFrameId).toHaveBeenCalledWith(frameId, startIndex, length);
-
+        expect(mockedEditorApi.selectTextById).toHaveBeenCalledTimes(1);
+        expect(mockedEditorApi.selectTextById).toHaveBeenCalledWith(frameId, startIndex, length);
     });
 });
