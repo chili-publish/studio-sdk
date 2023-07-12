af511f61fccbdc087dcc7581302f5667f4607b2c
Pieter Van Parys <pieter.vanparys@gmail.com>
2023-07-12 13:31:41 +0000

    Cleanup Media Connector Types

--- types/MediaConnector.d.ts
+++ types/MediaConnector.d.ts
@@ -1,39 +1,44 @@
 import { QueryOptions, Dictionary, ArrayBufferPointer, DownloadType } from "./Connector.Shared";
 
-declare module "grafx-studio-mediaconnector" {
+interface MediaConnector {
+    query(options: QueryOptions, context: Dictionary): Promise<MediaPage>;
+    detail(id: string): Promise<MediaDetail>;
+    download(id: string, previewType: DownloadType, context: Dictionary): Promise<ArrayBufferPointer>
+    upload(name: string, blob: ArrayBufferPointer): Promise<Media>
+    remove(id: string): Promise<boolean>
+    copy(id: string, newName: string): Promise<Media>
+    getQueryOptions(): string[] | null;
+    getDownloadOptions(): string[] | null;
+    getCapabilities(): MediaConnectorCapabilities;
+}
 
-    interface MediaConnector {
-        query(options: QueryOptions, context: Dictionary): Promise<MediaPage>;
-        download(id: string, previewType: DownloadType, context: Dictionary): Promise<ArrayBufferPointer>
-        upload(name: string, blob: Int8Array): Promise<Media>
-        remove(id: string): Promise<boolean>
-        copy(id: string, newName: string): Promise<Media>
-        getQueryOptions(): string[] | null;
-        getDownloadOptions(): string[] | null;
-        getCapabilities(): MediaConnectorCapabilities;
-    }
-    
-    type MediaConnectorCapabilities = {
-        filtering: boolean;
-        upload: boolean;
-        query: boolean;
-        remove: boolean;
-        copy: boolean;
-    }
+type MediaConnectorCapabilities = {
+    filtering: boolean;
+    upload: boolean;
+    query: boolean;
+    detail: boolean;
+    remove: boolean;
+    copy: boolean;
+}
 
-    interface MediaPage {
-        pageSize: number;
-        data: Media[];
-        links: {
-            nextPage: string;
-        };
-    }
+interface MediaPage {
+    pageSize: number;
+    data: Media[];
+    links: {
+        nextPage: string;
+    };
+}
 
-    interface Media {
-        id: String;
-        name: String;
-        relativePath: String;
-        type: number;
-        metaData: Dictionary;
-    }
+interface Media {
+    id: String;
+    name: String;
+    relativePath: String;
+    type: number;
+    metaData: Dictionary;
+}
+
+interface MediaDetail extends Media {
+    width?: number;
+    height?: number;
+    extension?: string;
 }
