bbbf6ea57175f96361c0884b4cacb31427b6cc09
Laurent Raymond <1528484+Dvergar@users.noreply.github.com>
2023-08-02 17:01:51 +0200

    [Feature] Update document & keep connectors

--- src/controllers/DocumentController.ts
+++ src/controllers/DocumentController.ts
@@ -30,13 +30,21 @@ export class DocumentController {
     };
 
     /**
-     * This method will load a provided document in the ChiliDocument format
+     * This method will load a provided document in the ChiliDocument format and
+     * clean old document states.
+     * If you set `keepConnectors` to true, `load` will keep your old connectors
+     * states e.g. options, authentication..
+     * By default `keepConnectors` is false and your connectors states are reset.
+     * In this case, you would need to configure your connectors appropariately
+     * beforehand.
      * @param doc the document to load in
+     * @param keepConnectors whether you want to keep previous connectors states
      * @returns the document loaded inside of the canvas
      */
-    load = async (doc: ChiliDocument | string) => {
+    load = async (doc: ChiliDocument | string, keepConnectors = false) => {
+        console.log('plop');
         const res = await this.#editorAPI;
-        if (typeof doc === 'string') return res.loadDocument(doc);
-        return res.loadDocument(JSON.stringify(doc)).then((result) => getEditorResponseData<null>(result));
+        if (typeof doc === 'string') return res.loadDocument(doc, keepConnectors);
+        return res.loadDocument(JSON.stringify(doc), keepConnectors).then((result) => getEditorResponseData<null>(result));
     };
 }
