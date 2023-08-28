5a6698266003fd454b3f6cc13e37f8002379af86
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2023-08-28 15:12:11 +0200

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const TerserPlugin = require('terser-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = (env) => ({
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    entry: {
+        index: path.resolve(__dirname, 'src', 'index.ts'),
+        actionHelpers: path.resolve(__dirname, 'src', 'actions', 'ActionHelpers.ts')
+    },
     output: {
         path: path.resolve(__dirname, '_bundles'),
         filename: '[name].js',
