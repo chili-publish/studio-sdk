2285028e7526832211d4d2833a540a72d1fa1b55
brapoprod <broels@chili-publish.com>
2023-09-07 17:34:09 +0200

    [Feature] Add InfoController

---
+++ src/controllers/InfoController.ts
@@ -0,0 +1,20 @@
+import engineInfo from '../../editor-engine.json';
+import sdkInfo from '../../package.json';
+
+/**
+ * The InfoController is responsible for getting info on the engine, like version f.e. .
+ * Methods and properties inside this controller can be accessed by `window.SDK.engine.{name}`
+ */
+export class InfoController {
+    /**
+     * This property returns the current engine version.
+     * @returns current engine version
+     */
+    currentEngineVersion = engineInfo.current;
+
+    /**
+     * This property returns the current engine version.
+     * @returns current engine version
+     */
+    currentSDKVersion = sdkInfo.version;
+}
