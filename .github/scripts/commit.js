f16f80cbbd9ad7a67dd7e36afbb80d9422759056
pietervp <pieter.vanparys@gmail.com>
2023-09-22 13:06:25 +0000

    Change commit script

--- .github/scripts/commit.js
+++ .github/scripts/commit.js
@@ -3,14 +3,26 @@ const child_process = require('child_process');
 // read version property
 const version = require('./common').getRootPackageVersion();
 
-async function run() {
+function runCommand(command, args) {
+    const spawned = child_process.spawnSync(command, args, { stdio: 'inherit' });
 
-    // use default node to spawn a git command
-    child_process.spawn('git', ['add', '**/**/package.json']);
-    child_process.spawn('git', ['add', 'package.json']);
+    if (spawned.error) {
+        console.error(`Error running ${command} ${args.join(' ')}:`, spawned.error);
+        process.exit(spawned.status);
+    }
 
-    // commit
-    child_process.spawn('git', ['commit', '-m', `CI: bumps version to ${version} [skip ci]`]);
+    if (spawned.status !== 0) {
+        console.error(`Command ${command} ${args.join(' ')} exited with status code ${spawned.status}`);
+        process.exit(spawned.status);
+    }
 }
 
-run();
+// use default node to spawn a git command
+console.info(`Adding all package.json files to git commit`);
+runCommand('git', ['add', '**/**/package.json']);
+console.info(`Adding root package.json file to git commit`);
+runCommand('git', ['add', 'package.json']);
+
+// commit
+console.info(`Committing changes to git`);
+runCommand('git', ['commit', '-m', `CI: bumps version to ${version} [skip ci]`]);
