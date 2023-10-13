c183f4e2e1d6368ba8523f362ddb37779765820b
Evan McGeek <emcgeek@chili-publish.com>
2023-10-13 16:37:15 +0300

    [EDT-1172] types updated

--- types/FontConnector.d.ts
+++ types/FontConnector.d.ts
@@ -1,39 +1,57 @@
-import { QueryOptions, Dictionary, ArrayBufferPointer, ConnectorConfigOptions, ConnectorCapabilities } from "./Connector.Shared";
+import {
+  ArrayBufferPointer,
+  ConnectorConfigOptions,
+  Dictionary,
+  FontConnectorCapabilities,
+  QueryOptions,
+} from "./Connector.Shared";
 
 declare module "grafx-studio-fontconnector" {
-    interface GrafxFontConnector {
-        detail(familyId, context: Dictionary): FontStyle;
-        query(options: QueryOptions, context: Dictionary): Promise<FontFamilyPage>;
-        download(styleId: string, context: Dictionary): Promise<ArrayBufferPointer>
-        preview(familyId: string, previewFormat: FontPreviewFormat, context: Dictionary): Promise<ArrayBufferPointer>
-        getConfigurationOptions(): ConnectorConfigOptions | null;
-        getCapabilities(): ConnectorCapabilities;
-    }
+  interface GrafxFontConnector {
+    detail(
+      connectorId: string,
+      fontFamilyId: string,
+      context: Dictionary
+    ): Promise<FontStyle[]>;
+    query(
+      connectorId: string,
+      queryOptions: QueryOptions,
+      context: Dictionary
+    ): Promise<FontFamilyPage>;
+    download(styleId: string, context: Dictionary): Promise<ArrayBufferPointer>;
+    preview(
+      familyId: string,
+      previewFormat: FontPreviewFormat,
+      context: Dictionary
+    ): Promise<ArrayBufferPointer>;
+    getConfigurationOptions(): ConnectorConfigOptions | null;
+    getCapabilities(): FontConnectorCapabilities;
+  }
 
-    enum FontPreviewFormat {
-        Square = 'square',
-        Line = 'line',
-    }
+  enum FontPreviewFormat {
+    Square = "square",
+    Line = "line",
+  }
 
-    interface FontFamilyPage {
-        pageSize: number;
-        data: FontFamily[];
-        links: {
-            nextPage: string;
-        };
-    }
+  interface FontFamilyPage {
+    pageSize: number;
+    data: FontFamily[];
+    links: {
+      nextPage: string;
+    };
+  }
 
-    interface FontFamily {
-        id: string;
-        name: string;
-        fontStylesCount: number;
-        extensions: string[];
-    }
+  interface FontFamily {
+    id: string;
+    name: string;
+    fontStylesCount: number;
+    extensions: string[];
+  }
 
-    interface FontStyle {
-        id: string;
-        name: string;
-        familyId: string;
-        familyName: string;
-    }
+  interface FontStyle {
+    id: string;
+    name: string;
+    familyId: string;
+    familyName: string;
+  }
 }
--- types/MediaConnector.d.ts
+++ types/MediaConnector.d.ts
@@ -1,41 +1,48 @@
-import { QueryOptions, Dictionary, ArrayBufferPointer, ConnectorConfigOptions, ConnectorCapabilities } from "./Connector.Shared";
+import {
+  ArrayBufferPointer,
+  ConnectorConfigOptions,
+  Dictionary,
+  MediaConnectorCapabilities,
+  QueryOptions,
+} from "./Connector.Shared";
 
 export interface MediaConnector {
-    detail(id: string, context: Dictionary): Promise<MediaDetail>;
-    query(options: QueryOptions, context: Dictionary): Promise<MediaPage>;
-    download(id: string, previewType: DownloadType, context: Dictionary): Promise<ArrayBufferPointer>
-    upload(name: string, blob: ArrayBufferPointer, context: Dictionary): Promise<Media>
-    remove(id: string, context: Dictionary): Promise<boolean>
-    copy(id: string, newName: string, context: Dictionary): Promise<Media>
-    getConfigurationOptions(): ConnectorConfigOptions | null;
-    getCapabilities(): ConnectorCapabilities;
+  detail(id: string, context: Dictionary): Promise<MediaDetail>;
+  query(options: QueryOptions, context: Dictionary): Promise<MediaPage>;
+  download(
+    id: string,
+    previewType: DownloadType,
+    context: Dictionary
+  ): Promise<ArrayBufferPointer>;
+  getConfigurationOptions(): ConnectorConfigOptions | null;
+  getCapabilities(): MediaConnectorCapabilities;
 }
 
 export enum DownloadType {
-    lowresWeb = 'lowresWeb',
-    highresWeb = 'highresWeb',
-    outputVideo = 'outputVideo',
-    outputPdf = 'outputPdf'
+  lowresWeb = "lowresWeb",
+  highresWeb = "highresWeb",
+  outputVideo = "outputVideo",
+  outputPdf = "outputPdf",
 }
 
 export interface MediaPage {
-    pageSize: number;
-    data: Media[];
-    links: {
-        nextPage: string;
-    };
+  pageSize: number;
+  data: Media[];
+  links: {
+    nextPage: string;
+  };
 }
 
 export interface Media {
-    id: string;
-    name: string;
-    relativePath: string;
-    type: number;
-    metaData: Dictionary;
-    extension?: string;
+  id: string;
+  name: string;
+  relativePath: string;
+  type: number;
+  metaData: Dictionary;
+  extension?: string;
 }
 
 export interface MediaDetail extends Media {
-    width?: number;
-    height?: number;
+  width?: number;
+  height?: number;
 }
