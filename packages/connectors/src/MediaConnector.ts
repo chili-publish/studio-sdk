a6aed681497d780e0c1f14ea67d04572e4183eff
pietervp <pieter.vanparys@gmail.com>
2023-10-27 14:46:00 +0000

    Bring MediaConnector definition up to date

--- packages/connectors/src/MediaConnector.ts
+++ packages/connectors/src/MediaConnector.ts
@@ -3,12 +3,18 @@ import { ArrayBufferPointer, ConnectorConfigValue, Dictionary, QueryOptions } fr
 export interface MediaConnector {
     detail(id: string, context: Dictionary): Promise<MediaDetail>;
     query(options: QueryOptions, context: Dictionary): Promise<MediaPage>;
-    download(id: string, previewType: DownloadType, context: Dictionary): Promise<ArrayBufferPointer>;
+    download(
+        id: string,
+        previewType: DownloadType,
+        intent: DownloadIntent,
+        context: Dictionary,
+    ): Promise<ArrayBufferPointer>;
     getConfigurationOptions(): ConnectorConfigValue[] | null;
     getCapabilities(): MediaConnectorCapabilities;
 }
 
-export type DownloadType = 'lowresWeb' | 'highresWeb' | 'outputVideo' | 'outputPdf';
+export type DownloadIntent = 'web' | 'print' | 'animation';
+export type DownloadType = 'thumbnail' | 'mediumres' | 'highres' | 'fullres' | 'original';
 
 export interface MediaPage {
     pageSize: number;
