3f505c798f452b56f61ec67204d8a2d426dec955
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-01-25 09:15:11 +0100

    [Fix] Return editor response on `setZoom` call (#410)
    fix: return editor response on `setZoom`

--- packages/sdk/src/tests/controllers/ToolController.test.ts
+++ packages/sdk/src/tests/controllers/ToolController.test.ts
@@ -27,49 +27,57 @@ describe('ToolController', () => {
     });
 
     it('sets the pointer tool', async () => {
-        await mockedToolController.setPointer();
+        const response = await mockedToolController.setPointer();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.SELECT);
+        expect(response.success).toBeTruthy();
     });
     it('sets the move tool', async () => {
-        await mockedToolController.setHand();
+        const response = await mockedToolController.setHand();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.HAND);
+        expect(response.success).toBeTruthy();
     });
 
     it('sets the zoom tool', async () => {
-        await mockedToolController.setZoom();
+        const response = await mockedToolController.setZoom();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.ZOOM);
+        expect(response.success).toBeTruthy();
     });
 
     it('sets the text  tool', async () => {
-        await mockedToolController.setTextFrame();
+        const response = await mockedToolController.setTextFrame();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.TEXT_FRAME);
+        expect(response.success).toBeTruthy();
     });
 
     it('sets the image tool', async () => {
-        await mockedToolController.setImageFrame();
+        const response = await mockedToolController.setImageFrame();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.IMAGE_FRAME);
+        expect(response.success).toBeTruthy();
     });
 
     it('sets the shape rect tool', async () => {
-        await mockedToolController.setShapeRect();
+        const response = await mockedToolController.setShapeRect();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.SHAPE_RECT);
+        expect(response.success).toBeTruthy();
     });
 
     it('sets the shape ellipse tool', async () => {
-        await mockedToolController.setShapeEllipse();
+        const response = await mockedToolController.setShapeEllipse();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.SHAPE_ELLIPSE);
+        expect(response.success).toBeTruthy();
     });
 
     it('sets the shape polygon tool', async () => {
-        await mockedToolController.setShapePolygon();
+        const response = await mockedToolController.setShapePolygon();
         expect(mockEditorApi.setTool).toHaveBeenCalledTimes(1);
         expect(mockEditorApi.setTool).toHaveBeenCalledWith(ToolType.SHAPE_POLYGON);
+        expect(response.success).toBeTruthy();
     });
 });
