7d5e44094331e3ac22ab246446a9492333dc9f2f
brapoprod <broels@chili-publish.com>
2024-03-08 11:49:57 +0100

    Add typescript example

---
+++ examples/sdk/basic-typescript-example/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/integration.ts",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
