a47d368ef18799b6c892150eabc707b0866b9437
Nicola Verbeeck <nicola@chimerapps.com>
2024-03-22 15:39:00 +0100

    [EDT-1429][EDT-1393] Add missing barcode properties

--- packages/sdk/src/types/FrameTypes.ts
+++ packages/sdk/src/types/FrameTypes.ts
@@ -123,6 +123,8 @@ export type BarcodeFrame = {
         backgroundColor: ColorUsage;
         enableBarColor: boolean;
         barColor: ColorUsage;
+        enableText: boolean;
+        barHeight: number;
     };
     src: BarcodeSource;
     barcodeType: BarcodeType;
