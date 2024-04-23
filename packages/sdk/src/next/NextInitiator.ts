69443ed634cbf652e294dd5382214319155f435c
brapoprod <broels@chili-publish.com>
2024-04-23 15:54:15 +0200

    Add docs

--- packages/sdk/src/next/NextInitiator.ts
+++ packages/sdk/src/next/NextInitiator.ts
@@ -1,7 +1,15 @@
 import { Connection } from 'penpal';
 import { ConfigType, EditorAPI } from '../types/CommonTypes';
 import { SubscriberController } from './controllers/SubscriberController';
+import { VariableController } from './controllers/VariableController';
 
+/**
+ * The next initiator is a split off on SDK level to tag next features.
+ * The principle is that all breaking changes that we are anticipating, come into this section of the SDK.
+ * This allows us to introduce breaking changes before bumping the SDK Major version, since we want to bundle some of them.
+ * This way our major version can stay stable for longer, without sacrificing the flexibility we need to innovate.
+ * Together with a major update, the functions and additions in this part of the SDK will be moved to the main part.
+ */
 export class NextInitiator {
     private config: ConfigType;
     private connection: Connection;
@@ -12,6 +20,7 @@ export class NextInitiator {
     private editorAPI: EditorAPI;
 
     subscriber: SubscriberController;
+    variable: VariableController;
 
     /**
      * The next initiator is a split off on SDK level to tag next features
@@ -24,5 +33,6 @@ export class NextInitiator {
         this.editorAPI = editorAPI;
 
         this.subscriber = new SubscriberController(config);
+        this.variable = new VariableController(this.editorAPI);
     }
 }
