321c90f3bc6c2d034eece9ea4af93a45b6a2ab70
Brian Roels <54845569+brapoprod@users.noreply.github.com>
2024-06-26 08:52:59 +0200

    [Feature] Add AQA test example (#475)

---
+++ examples/sdk/aqa-typescript-auth-example/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.ts",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
