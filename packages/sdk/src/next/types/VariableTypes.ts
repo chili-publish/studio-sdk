afd8f3d207bbbc5055addee167ffb56bf84c6072
Alexandra Bri <popalexandra45@gmail.com>
2024-07-31 16:31:34 +0300

    fix ListVariable type

--- packages/sdk/src/next/types/VariableTypes.ts
+++ packages/sdk/src/next/types/VariableTypes.ts
@@ -1,8 +1,10 @@
-import { ListVariableItem, Variable } from '../../types/VariableTypes';
+import { ListVariableItem, ValueWithStyle, Variable } from '../../types/VariableTypes';
 
 export interface ListVariable extends Variable {
     items: ListVariableItem[];
     selected?: ListVariableItem;
+    prefix?: ValueWithStyle;
+    suffix?: ValueWithStyle;
 }
 
 export enum VariableType {
