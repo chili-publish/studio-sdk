6294ebc49141557e8a8c7ddbda3d1fea938f903f
sebastian-mereuta <sebastian.mereuta93@gmail.com>
2024-12-20 11:47:08 +0200

    WRS-2184 Cached login credentials and automatic redirect to login if not authenticated

--- examples/sdk/aqa-typescript-auth-example/src/app.ts
+++ examples/sdk/aqa-typescript-auth-example/src/app.ts
@@ -1,10 +1,9 @@
 import integration from "./integration";
-import { createAuth0Client } from "@auth0/auth0-spa-js";
+import { Auth0Client, createAuth0Client } from "@auth0/auth0-spa-js";
 
-// Instantiate the class when the window loads
-window.onload = async () => {
-  let isAuthenticated = false;
-  const auth0 = await createAuth0Client({
+// init client
+async function initializeAuth0() {
+  return await createAuth0Client({
     domain: "https://login.chiligrafx-dev.com",
     clientId: "zkWRMzeEKNSoAvJ8hj4Od3sp7pUFtXOO",
     authorizationParams: {
@@ -13,43 +12,40 @@ window.onload = async () => {
       redirect_uri: window.location.href,
     },
     useRefreshTokens: true,
+    cacheLocation: "localstorage",
   });
+}
 
-  const loginButton = document.getElementById("login");
-  if (loginButton) {
-    loginButton.addEventListener("click", async (e: Event) => {
-      await auth0.loginWithRedirect({
-        ...(typeof window !== "undefined" && {
-          appState: {
-            returnTo: window.location.href,
-          },
-        }),
-        authorizationParams: {
-          prompt: "login",
-          redirect_uri: window.location.href,
-        },
-      });
-    });
-  }
-
+// handle redirect callback
+async function handleRedirectCallback(auth0: Auth0Client) {
   if (
     location.search.includes("state=") &&
     (location.search.includes("code=") || location.search.includes("error="))
   ) {
-    // Handle the redirect from Auth0
     await auth0.handleRedirectCallback();
-
-    isAuthenticated = await auth0.isAuthenticated();
-
-    if (isAuthenticated) {
-      document.getElementById("login").remove();
-      (<HTMLInputElement>document.getElementById("init")).disabled = false;
-    }
+    window.history.replaceState({}, document.title, window.location.origin);
+    return await auth0.isAuthenticated();
   }
+  return false;
+}
 
+// redirect login
+async function redirectToLogin(auth0: Auth0Client) {
+  await auth0.loginWithRedirect({
+    appState: {
+      returnTo: window.location.href,
+    },
+    authorizationParams: {
+      prompt: "login",
+      redirect_uri: window.location.href,
+    },
+  });
+}
+
+function initializePRPage(isAuthenticated: boolean, auth0: Auth0Client) {
   const initButton = document.getElementById("init");
   if (initButton) {
-    initButton.addEventListener("click", async (e: Event) => {
+    initButton.addEventListener("click", async () => {
       if (isAuthenticated) {
         const pr = (<HTMLInputElement>document.getElementById("pr")).value;
         const accessToken = await auth0.getTokenSilently();
@@ -57,4 +53,21 @@ window.onload = async () => {
       }
     });
   }
+}
+
+window.onload = async () => {
+  const auth0 = await initializeAuth0();
+
+  // redirect flow
+  const isAuthenticated =
+    (await auth0.isAuthenticated()) || (await handleRedirectCallback(auth0));
+
+  // login if not authenticated
+  if (!isAuthenticated) {
+    await redirectToLogin(auth0);
+    return;
+  }
+
+  (<HTMLInputElement>document.getElementById("init")).disabled = false;
+  initializePRPage(isAuthenticated, auth0);
 };
