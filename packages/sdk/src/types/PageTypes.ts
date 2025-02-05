a312c9ae02327cc26d78a786a8acc6b012364772
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2025-02-05 13:56:22 +0100

    EDT-1817 define max time to wait for assets to be loaded before generating the snapshot.
    Update default value

--- packages/sdk/src/types/PageTypes.ts
+++ packages/sdk/src/types/PageTypes.ts
@@ -23,4 +23,12 @@ export interface SnapshotSettings {
      * - The maximum allowed value is 1000 pixels.
      */
     largestAxisSize?: number | null;
+    /**
+     * The maximum time to wait in milliseconds for the assets to load before the snapshot is taken.
+     * 
+     * When the time is exceeded, the snapshot will be taken even if not all assets are loaded yet.
+     * 
+     * When not defined, the default value is 10000 ms.
+     */
+    maxWaitTimeMs?: number | null;
 }
