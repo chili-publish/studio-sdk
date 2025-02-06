0bd9e5008e46b48992eeb08ecf6563a4c119c1f3
Evan McGeek <emcgeek@chili-publish.com>
2025-02-06 20:54:09 +0700

    [EDT-1765] comment on largestAxisSize property

--- packages/sdk/src/types/PageTypes.ts
+++ packages/sdk/src/types/PageTypes.ts
@@ -15,5 +15,11 @@ export type PageSize = {
 };
 
 export interface SnapshotSettings {
+    /**
+     * The maximum size of the snapshot's longest dimension in pixels.
+     * - If set to a number (e.g., 100), the longest side of the snapshot will be limited to that value.
+     * - If set to `null` or ignored, the snapshot will use the full page dimensions.
+     * - The maximum allowed value is 1000 pixels.
+     */
     largestAxisSize?: number | null;
 }
