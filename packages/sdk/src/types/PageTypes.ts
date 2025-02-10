b0093d7a338d6c88a2256f09d9168879c1d6774b
Evan McGeek <74073782+evan-mcgeek@users.noreply.github.com>
2025-02-10 16:41:33 +0700

    [EDT-1765] SnapshotSettings introduced (#549)
    * [EDT-1765] SnapshotSettings introduced

--- packages/sdk/src/types/PageTypes.ts
+++ packages/sdk/src/types/PageTypes.ts
@@ -13,3 +13,14 @@ export type PageSize = {
     width: number;
     height: number;
 };
+
+export interface SnapshotSettings {
+    /**
+     * The maximum size of the snapshot's longest dimension in pixels.
+     * - If set to a number (e.g., 100), the longest side of the snapshot will be limited to that value.
+     * - In case when layout size is smaller than the specified value, the snapshot will be upscaled.
+     * - If set to `null` or ignored, the snapshot will use the full page dimensions.
+     * - The maximum allowed value is 1000 pixels.
+     */
+    largestAxisSize?: number | null;
+}
