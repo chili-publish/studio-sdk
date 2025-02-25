2c5408e2675d7aa91534023e77cb188c259aa54f
Evan McGeek <emcgeek@chili-publish.com>
2025-02-25 15:43:27 +0700

    [EDT-1853] compatibility

--- packages/sdk/src/next/types/CommonTypes.ts
+++ packages/sdk/src/next/types/CommonTypes.ts
@@ -1,10 +1,8 @@
 import { EngineEvent } from '../../utils/EngineEvent';
 import { PageSize } from './PageTypes';
-import { ConfigType, MaybePromise } from '../../types/CommonTypes';
+import { MaybePromise } from '../../types/CommonTypes';
 import { Variable } from '../../types/VariableTypes';
 import { ConnectorInstance } from '../../types/ConnectorTypes';
-
-export type RuntimeConfigType = ConfigType & ManagedCallbacksConfigType;
 
 export type ManagedCallbacksConfigType = {
     events: {
