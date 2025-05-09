30b0b0e38bbfa3cc2ba89b04994a992960588e17
Nicola Verbeeck <nicola@chimerapps.com>
2025-05-09 09:48:54 +0200

    [Feature][EDT-1963] Add set page size method

--- packages/sdk/src/next/controllers/PageController.ts
+++ packages/sdk/src/next/controllers/PageController.ts
@@ -28,7 +28,7 @@ export class PageController {
     /**
      * This method will set the width of all pages to a specific value.
      * This only works if the document is a project.
-     * @param width the string value that will be calculated (f.e. 1+1 will result in 2) The notation is in pixels
+     * @param width the string value that will be calculated (f.e. 1+1 will result in 2)  The default unit is in the current layout unit (e.g. px, mm, in)
      * @returns
      */
     setWidth = async (width: string) => {
@@ -39,11 +39,22 @@ export class PageController {
     /**
      * This method will set the height of all pages to a specific value.
      * This only works if the document is a project.
-     * @param height the string value that will be calculated (f.e. 1+1 will result in 2). The notation is in pixels
+     * @param height the string value that will be calculated (f.e. 1+1 will result in 2).  The default unit is in the current layout unit (e.g. px, mm, in)
      * @returns
      */
     setHeight = async (height: string) => {
         const res = await this.#editorAPI;
         return res.setPageHeight(height).then((result) => getEditorResponseData<null>(result));
+    };
+
+    /**
+     * Sets the width and height of the page to a specific value simultaneously.
+     * This only works if the document is a project.
+     * @param width the string value that will be calculated (f.e. 1+1 will result in 2). The default unit is in the current layout unit (e.g. px, mm, in)
+     * @param height the string value that will be calculated (f.e. 1+1 will result in 2). The default unit is in the current layout unit (e.g. px, mm, in)
+     */
+    setSize = async (width: string, height: string) => {
+        const res = await this.#editorAPI;
+        return res.setPageSize(width, height).then((result) => getEditorResponseData<null>(result));
     };
 }
