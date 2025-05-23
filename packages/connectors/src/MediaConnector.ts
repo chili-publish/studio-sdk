bf5c07401570cd96e1f5347c8b841d8ed1d2fbd5
Pavel Samusev <psamusev@chili-publish.com>
2025-05-23 17:21:43 +0300

    chore: simplify media asset types

--- packages/connectors/src/MediaConnector.ts
+++ packages/connectors/src/MediaConnector.ts
@@ -30,7 +30,10 @@ export interface MediaConnectorUpload {
 
 export type DownloadIntent = 'web' | 'print' | 'animation';
 export type DownloadType = 'thumbnail' | 'mediumres' | 'highres' | 'fullres' | 'original';
-export type MediaType = 0 | 1;
+
+export type FileType = 0;
+export type FolderType = 1;
+export type MediaType = FileType | FolderType;
 
 export type MediaPage = QueryPage<Media>;
 export type Media = GenericMedia<MediaType>;
