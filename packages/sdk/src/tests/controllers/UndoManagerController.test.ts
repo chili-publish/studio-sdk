0e1bc5e639de4b1dbd629e7bb9d0187a0be4fc67
Ana Srbljanin <asrbljanin@chili-publish.com>
2026-02-19 12:22:25 +0100

    [EDT-2293] Fixed test

--- packages/sdk/src/tests/controllers/UndoManagerController.test.ts
+++ packages/sdk/src/tests/controllers/UndoManagerController.test.ts
@@ -20,6 +20,7 @@ describe('UndoManagerController', () => {
         abort: async () => getEditorResponseData(castToEditorResponse(null)),
         pause: async () => getEditorResponseData(castToEditorResponse(null)),
         resume: async () => getEditorResponseData(castToEditorResponse(null)),
+        clear: async () => getEditorResponseData(castToEditorResponse(null)),
     };
 
     beforeEach(() => {
@@ -34,6 +35,7 @@ describe('UndoManagerController', () => {
         jest.spyOn(mockEditorApi, 'abort');
         jest.spyOn(mockEditorApi, 'pause');
         jest.spyOn(mockEditorApi, 'resume');
+        jest.spyOn(mockEditorApi, 'clear');
     });
 
     afterEach(() => {
