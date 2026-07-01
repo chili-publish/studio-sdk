852bb43cab3850570d6e1ceba307868911829a2e
asrbljaninchili <asrbljanin@chili-publish.com>
2026-07-01 13:52:22 +0200

    [EDT-2368] componentFit from frame to FrameLayoutType for component grids (#791)
    EDT-2368 Moved componentFit from frame to FrameLayoutType for component grids

--- packages/sdk/src/types/FrameTypes.ts
+++ packages/sdk/src/types/FrameTypes.ts
@@ -39,6 +39,7 @@ export type FrameLayoutType = {
     customCroppedAssetCount: number;
     subjectId: PropertyState<Id | null>;
     componentGridSettings: PropertyState<ComponentGridSettings>;
+    componentFit: PropertyState<ComponentFitEnum>;
 } | null;
 
 //Frame.image
@@ -203,11 +204,6 @@ export type ComponentGridFrame = {
     dataSourceVariableId?: Id | null;
     src?: ComponentSource | null;
     variableMappings: GridVariableMapping;
-
-    /**
-     * How the component grid fits within the frame.
-     */
-    fitMode: ComponentFitEnum;
 };
 
 export type GridVariableMapping = {
